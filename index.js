3a7e36760829da9bc8e67b7cec92e1ef4d452f8c
Aika Sat <aikasat95@gmail.com>
2020-02-21 18:11:06 +0600

    -refactored index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const mysqlDb = require('./mysqlDb');
-const controllers = require('./controllers/index');
+const controllers = require('./controllers');
 
 const app = express();
 const port = 8000;
